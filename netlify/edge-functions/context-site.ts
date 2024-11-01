bac19f2d940ca6e9ff257e771d3758e59838a9c8
Robin Wong <chaowang0313@gmail.com>
2024-11-02 01:58:19 +0800

    feat: test

--- netlify/edge-functions/context-site.ts
+++ netlify/edge-functions/context-site.ts
@@ -1,7 +1,11 @@
 import type { Context, Config } from "@netlify/edge-functions";
 
 export default async (request: Request, context: Context) => {
-  return new Response(`Hello from ${context.site.name}!`);
+  return new Response(`Hello from ${context.server.region}!`, {
+    headers: {
+      'x-probe-region': context.server.region,
+    },
+  });
 };
 
 export const config: Config = {
